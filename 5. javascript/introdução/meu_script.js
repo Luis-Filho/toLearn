f1271984d8f8c924b916d37da3b16edd9f9afc1c
Luis Costa <luis.filho@outlook.com.br>
2020-11-17 20:33:13 -0300

    um bucado de coisa que eu fiz e esqueci de mandar

---
+++ 5. javascript/introdução/meu_script.js
@@ -0,0 +1,8 @@
+//alert('Ola seja bem vindo ao curso')
+
+/*
+  a função responsavel por resgatar um elemento da DOM 
+  e atribuir uma string a ela
+ */
+
+document.getElementById("nome").value = "Luis costa";
