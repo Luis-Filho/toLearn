aa8fc01268d2ae24d92c50fef1b672be882a80ab
Luis Costa <luis.filho@outlook.com.br>
2020-11-18 00:06:49 -0300

    Alteração de dificuldade

--- 5. javascript/mata mosquito/jogo.js
+++ 5. javascript/mata mosquito/jogo.js
@@ -3,6 +3,17 @@ var largura = 0
 var vidas = 1
 var tempo = 10
 
+var criaMosquitoTempo = 1500
+var nivel = window.location.search
+nivel = nivel.replace('?', '')
+
+if (nivel === 'normal')
+  criaMosquitoTempo = 1500
+else if (nivel === 'dificil')
+  criaMosquitoTempo = 1000
+else
+  criaMosquitoTempo = 750
+
 function ajustaTamanhoPalcoJogo() {
   altura = window.innerHeight
   largura = window.innerWidth
@@ -10,32 +21,32 @@ function ajustaTamanhoPalcoJogo() {
 
 ajustaTamanhoPalcoJogo();
 
-var cronometro = setInterval(function(){
+var cronometro = setInterval(function () {
   tempo -= 1
 
-  if (tempo < 0){
+  if (tempo < 0) {
     clearInterval(cronometro)
     clearInterval(criaMosquito)
     window.location.href = 'vitoria.html'
   }
-  else 
-    document.getElementById('cronometro').innerHTML = tempo  
-},1000)
+  else
+    document.getElementById('cronometro').innerHTML = tempo
+}, 1000)
 
 console.log(altura, largura)
 
 function posicaoRandomica() {
   //remover o mosquito anterior (caso Exista)
-  if (document.getElementById('mosquito')){
+  if (document.getElementById('mosquito')) {
     document.getElementById('mosquito').remove();
-    
-    if (vidas > 3){      
+
+    if (vidas > 3) {
 
       window.location.href = 'fim_de_jogo.html'
-    } else 
+    } else
       document.getElementById('v' + vidas++).src = "imagens/coracao_vazio.png"
   }
-    
+
 
   var posicaoX = Math.floor(Math.random() * largura) - 90
   var posicaoY = Math.floor(Math.random() * altura) - 90
@@ -53,8 +64,8 @@ function posicaoRandomica() {
   mosquito.style.top = posicaoY + 'px'
   mosquito.style.position = 'absolute'
   mosquito.id = 'mosquito'
-  mosquito.onclick = function (){
-    this.remove() 
+  mosquito.onclick = function () {
+    this.remove()
   }
 
   document.body.appendChild(mosquito)
