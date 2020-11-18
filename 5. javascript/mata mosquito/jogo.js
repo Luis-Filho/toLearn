ddb6ea0b2952215e40c707af15945cc3e1861307
Luis Costa <luis.filho@outlook.com.br>
2020-11-17 23:32:48 -0300

    Tela de game over e botão de reiniciar o jogo

--- 5. javascript/mata mosquito/jogo.js
+++ 5. javascript/mata mosquito/jogo.js
@@ -16,8 +16,9 @@ function posicaoRandomica() {
   if (document.getElementById('mosquito')){
     document.getElementById('mosquito').remove();
     
-    if (vidas > 3){
-      alert("game over!")
+    if (vidas > 3){     
+
+      window.location.href = 'fim_de_jogo.html'
     } else 
       document.getElementById('v' + vidas++).src = "imagens/coracao_vazio.png"
   }
