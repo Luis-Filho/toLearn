708b726914d165cb123aa978ba5aa8a61db2bbea
Luis Costa <luis.filho@outlook.com.br>
2020-11-17 23:19:27 -0300

    Fluxo de vida adicionado.

--- 5. javascript/mata mosquito/jogo.js
+++ 5. javascript/mata mosquito/jogo.js
@@ -1,5 +1,6 @@
 var altura = 0
 var largura = 0
+var vidas = 1
 
 function ajustaTamanhoPalcoJogo() {
   altura = window.innerHeight
@@ -12,8 +13,15 @@ console.log(altura, largura)
 
 function posicaoRandomica() {
   //remover o mosquito anterior (caso Exista)
-  if (document.getElementById('mosquito'))
+  if (document.getElementById('mosquito')){
     document.getElementById('mosquito').remove();
+    
+    if (vidas > 3){
+      alert("game over!")
+    } else 
+      document.getElementById('v' + vidas++).src = "imagens/coracao_vazio.png"
+  }
+    
 
   var posicaoX = Math.floor(Math.random() * largura) - 90
   var posicaoY = Math.floor(Math.random() * altura) - 90
@@ -31,6 +39,9 @@ function posicaoRandomica() {
   mosquito.style.top = posicaoY + 'px'
   mosquito.style.position = 'absolute'
   mosquito.id = 'mosquito'
+  mosquito.onclick = function (){
+    this.remove() 
+  }
 
   document.body.appendChild(mosquito)
 }
